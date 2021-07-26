00108646c9638e852466f63259a9fb19fdffab23
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2021-07-26 10:27:40 +0500

    clock

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Stopwatch from './Components/Stopwatch';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Loading from './Components/Loading';
+import ClockWithHook from './Components/ClockWithHook';
 
 class App extends React.Component {
-  state = {showClock: true};
+  state = {showClock: false};
 
   toggleClock = ()=>{
     this.setState((state) => {
@@ -19,8 +20,9 @@ class App extends React.Component {
         {this.state.showClock && "Hide" || "Show"}
       </button>
       {this.state.showClock && <Stopwatch />}
+        <div className="mt-4">{this.state.showClock && <ClockWithHook />}</div>
       <div className="text-center mt-4">
-        <Loading />
+        {this.state.showClock && <Loading />}
       </div>
     </div>
   }
--- src/Components/ClockWithHook.js
+++ src/Components/ClockWithHook.js
@@ -1,1 +1,19 @@
-const ClockWithHook
+import { useEffect, useState } from "react"
+
+const setZero = n => n < 10 ? "0" + n : n;
+
+const ClockWithHook = () => {
+    const [time, setTime] = useState(new Date());
+
+    const hour =setZero(time.getHours());
+    const minute =setZero(time.getMinutes());
+    const seconds =setZero(time.getSeconds());
+
+    useEffect(() => {
+        setTimeout(() => {setTime(new Date())}, 1000)
+    }, [time])
+
+    return <h1>{hour}:{minute}:{seconds}</h1>
+}
+
+export default ClockWithHook;
